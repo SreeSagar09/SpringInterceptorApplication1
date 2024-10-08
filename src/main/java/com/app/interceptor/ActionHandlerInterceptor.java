34688dff0430e573170f296269dd05e6c39cf768
SreeSagar09 <sreesagarmamidala09@gmail.com>
2024-10-08 23:38:33 +0530

    SpringInterceptorApplication1 application initial commit.

---
+++ src/main/java/com/app/interceptor/ActionHandlerInterceptor.java
@@ -0,0 +1,32 @@
+package com.app.interceptor;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.handler.HandlerInterceptorAdapter;
+
+public class ActionHandlerInterceptor extends HandlerInterceptorAdapter {
+	
+	@Override
+	public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
+		System.out.println("From preHandle method in ActionHandlerInterceptor class");
+		return true;
+		
+	}
+	
+	@Override
+	public void postHandle(HttpServletRequest request, HttpServletResponse response, Object handler, ModelAndView modelAndView) throws Exception {
+		System.out.println("From postHandle method in ActionHandlerInterceptor class");
+	}
+	
+	@Override
+	public void afterCompletion(HttpServletRequest request, HttpServletResponse response, Object handler, Exception ex) throws Exception {
+		System.out.println("From afterCompletion method in ActionHandlerInterceptor class");
+	}
+	
+	@Override
+	public void afterConcurrentHandlingStarted(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
+		System.out.println("From afterConcurrentHandlingStarted method in ActionHandlerInterceptor class");
+	}
+}
